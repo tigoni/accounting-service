715c587aaa5dd3ac97cbe97d88f2a0425a6a70ac
KM <kmuhindi@proton.me>
2025-12-05 00:54:48 +0300

    Initialize accounting service with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/pezesha/taskproject/accounting_service/AccountingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pezesha.taskproject.accounting_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingServiceApplication.class, args);
+	}
+
+}
